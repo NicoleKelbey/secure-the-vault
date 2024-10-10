99833a299a5c76e04a7206e4cf5c856cbc09e26d
NicoleKelbey <nikki.markert@yahoo.com>
2024-10-10 09:04:04 -0400

    Secure the vault

---
+++ index.js
@@ -0,0 +1,13 @@
+/* Pseudocode
+1. Creat a string for the user : You have received this message because you have been chosen to open an important vault. Here is the secret combination:
+2.Assign three variables. The variables will equal the combination using an arithmic operator.
+3.Display the message in an alert popup.
+*/
+
+let userOpen = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+
+let num1 = 2*5 // 10;
+const num2 = 80-40 // 40;
+const num3 = 38+1 // 39;
+
+alert(`${userOpen} ${firstNum} - ${secondNumber} - ${thirdNumber}.`)
